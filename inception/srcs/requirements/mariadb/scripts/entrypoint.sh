8a81aef891af52e8326fd6978f06fdc7ee50a619
waxcoin <waxcoin@hotmail.com>
2022-06-11 01:16:53 +0200

    feat: wordpress run need add pwd root mariabd et acces databases of user

--- inception/srcs/requirements/mariadb/scripts/entrypoint.sh
+++ inception/srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -1,37 +1,14 @@
 #!/bin/sh
 # create file setting for mysql
-cat << EOF > setting.sql
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
-
-CREATE DATABASE IF NOT EXISTS $MARIADB_DB;
-CREATE USER IF NOT EXISTS  '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PWD';
-GRANT ALL privileges ON $MARIADB_DB.* TO $MARIADB_USER@'localhost';
-FLUSH PRIVILEGES;
-EOF
-
-rm .setup 2>/dev/null
-if [ $? -ne 0 ]
-then
-	# check if mysql running if not start service
-	services=$(service mysql status)
-	if [[ ${services} = *stopped* ]]
-	then
-		service mysql start
-	fi
-	# Apply config
-	sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb.cnf
-	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb.cnf
-
-	# push setting in mysql
-	echo "$(cat setting.sql)" | mariadb
-	pkill mariadb
-	touch .setup
+if [ ! -d "/var/lib/mysql/wordpress" ]; then 
+    
+    mysql_install_db
+    service mysql start
+    
+    mysql -e "CREATE USER '${MARIADB_USER}'@'localhost' identified by '${MARIADB_PWD}';" &&\
+	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PWD}';" &&\
+	mysql -e "FLUSH PRIVILEGES;"
+    service mysql stop 
 fi
-
 mysqld_safe --datadir=/var/lib/mysql
-
--- inception/srcs/requirements/wordpress/script/entrypoint.sh
+++ inception/srcs/requirements/wordpress/script/entrypoint.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 
-target="/etc/php7/php-fpm.d/www.conf"
-
-grep -E "listen = 127.0.0.1" $target > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-	sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
-	echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
-	echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
-	echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
-	echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
-fi
-
-if [ ! -f "wp-config.php" ]; then
-	cp /config/wp-config ./wp-config.php
-
-	sleep 5;
-	if ! mysqladmin -h $MARIADB_HOST -u $MARIADB_USER \
-		--password=$MARIADB_PWD --wait=60 ping > /dev/null; then
-		printf "MySQL is not available.\n"
-		exit 1
-	fi
-wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
-	--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
-wp plugin update --all
-wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
-fi
-
-php-fpm7 --nodaemonize
+if  [ ! -f /var/www/wordpress/wp-config.php ]; then 
+    wp core --allow-root download --locale=fr_FR --force 
+    sleep 5;
+    while  [ ! -f /var/www/wordpress/wp-config.php ]; do   
+        wp core config --allow-root --dbname=wordpress --dbuser=$MARIADB_USER --dbpass=$MARIADB_PWD --dbhost=mariadb:3306
+    done 
+    wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD  --admin_email=$WP_ADMIN_EMAIL --path='/var/www/wordpress';
+    wp  user create --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_ADMIN_PWD --role=author
+    wp theme install --allow-root dark-mode --activate    
+fi 
+php-fpm7.3 --nodaemonize
