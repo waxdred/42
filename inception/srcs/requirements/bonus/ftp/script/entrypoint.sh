0e36c127d8ecd1ece389fc51d3f4e66e6a3428e3
jmilhas <waxdred@protonmail.com>
2022-06-14 16:36:27 +0200

    feat: update ftp still don't work

--- inception/srcs/requirements/bonus/ftp/script/entrypoint.sh
+++ inception/srcs/requirements/bonus/ftp/script/entrypoint.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
 
+adduser $FTP_USR --disabled-password
+echo -e "${FTP_USR}\n${FTP_PWD}" > /etc/vsftpd/vsftpd.userlist
+
+chown -R ${FTP_USR}:${FTP_PWD} /var/www/wordpress
+cat << EOB
+	*************************************************
+	*                                               *
+	*    Docker image: fauria/vsftpd                *
+	*    https://github.com/fauria/docker-vsftpd    *
+	*                                               *
+	*************************************************
+	SERVER SETTINGS
+	---------------
+	· FTP User: $FTP_USR
+	· FTP Password: $FTP_PWD
+	· Redirect vsftpd log to STDOUT: No.
+EOB
+
+vsftpd /etc/vsftpd/vsftpd.conf
+tail -f /dev/null
